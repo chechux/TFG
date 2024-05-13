360ea7ce0b37e4c7b5657a561a6d1d9035fc8c4a
Jesús Nieto <jnietoribera@gmail.com>
2024-05-14 00:06:57 +0200

    añadimos funcion de eliminar productos de una lista y añadimos estilos al main

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const bcryptjs = require("bcryptjs");
 const connection = require("../database/db")
-const Swal = require('sweetalert2')
 
 
 
@@ -38,7 +37,6 @@ router.get("/register",(req,res,next)=>{
 
 router.get("/main", async (req,res,next)=>{
   const [producto] = await connection.query('SELECT * FROM productos')
-  const [listas] = await connection.query('SELECT * FROM listas')
   res.render("main",{producto})
 })
 
@@ -49,8 +47,7 @@ router.get("/editar/:id",async (req,res,next)=>{
   const {id} = req.params
   const [tabla] = await connection.query('SELECT * FROM listas WHERE id=?',[id])
   const [productos] = await connection.query('SELECT p.* FROM productos p INNER JOIN listas_productos lp ON p.id = lp.id_productos WHERE lp.id_lista =?',[id])
-  res.render("edit",{tabla:tabla[0], productos})
-  console.log(tabla)
+  res.render("edit",{tabla:tabla[0], productos,})
 
 })
 
@@ -58,21 +55,18 @@ router.post("/editar/:id",async(req,res,next)=>{
   const {id} = req.params
   const {nombre} = req.body
   const new_nombre ={nombre}
+  const id_producto = req.body.listas
   await connection.query('UPDATE listas SET ? WHERE id =?',[new_nombre,id])
+  await connection.query('DELETE FROM listas_productos WHERE id_productos = ?',[id_producto])
   res.redirect("/tablas")
+
 })
 
 
 router.get('/delete/:id', async (req, res) => {
   const {id} = req.params
+  await connection.query('DELETE FROM listas_productos WHERE id_lista = ?',[id])
   await connection.query('DELETE FROM listas WHERE id = ?', [id])
-  res.redirect("/tablas")
-})
-
-
-router.get("/eliminarProducto/:id", async(req,res)=>{
-  const {id} = req.params
-  await connection.query('DELETE FROM listas_productos WHERE id=?',[id])
   res.redirect("/tablas")
 })
 
@@ -80,7 +74,7 @@ router.get("/eliminarProducto/:id", async(req,res)=>{
 router.get("/add/:id",async(req,res)=>{
   const id_producto = req.params.id;
   const [listas] = await connection.query('SELECT * FROM listas')
-  // await connection.query('INSERT INTO listas_productos(id_productos) VALUES (?)',[id_producto])
+  await connection.query('INSERT INTO listas_productos(id_productos) VALUES (?)',[id_producto])
   res.render("add",{listas,id_producto})
 
 })
@@ -95,21 +89,13 @@ router.post("/add/:id",async(req,res,next)=>{
 })
 
 
-// listas.forEach(lista => {
-  //   console.log(lista.id);
-  // })
-
-
-
-
-
 router.post('/register', async (req, res) => {
 
   const { username, pass, gmail, edad } = req.body;
 
   if(0 < req.body.edad && req.body.edad > 18){
 
-    await connection.execute('INSERT INTO usuarios (username, pass, gmail, edad) VALUES (?, ?, ?, ?)', [username, pass,gmail,edad]);
+    await connection.query('INSERT INTO usuarios (username, pass, gmail, edad) VALUES (?, ?, ?, ?)', [username, pass,gmail,edad]);
     res.redirect("/main")
     
   }else{
@@ -137,10 +123,14 @@ router.post('/login', async (req, res) => {
 
 router.post("/crearTabla",async(req,res,next)=>{
   const nombre = req.body.nombre
-  await connection.execute('INSERT INTO listas (nombre) VALUES (?)',[nombre])
+  await connection.execute('INSERT INTO listas  (nombre) VALUES (?)',[nombre])
 
   res.redirect("/tablas")
   console.log(nombre)
+
+  // const idUsuario = usuarioActual.id; // Suponiendo que tienes el usuario actual disponible
+  //       await connection.query('INSERT INTO listas (id_usuario, nombre) VALUES (?, ?)', [idUsuario, nombreLista]);
+  //       res.redirect('/listas');
 })
 
 

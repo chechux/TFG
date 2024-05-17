60180de9abdc7105cfa642523700d276caf4e104
Jesús Nieto <jnietoribera@gmail.com>
2024-05-17 19:04:39 +0200

    añadimos los mismos estilos a otro popup

--- public/javascripts/popupTabla.js
+++ public/javascripts/popupTabla.js
@@ -1,14 +1,27 @@
-function abrirPopup() {
-    history.pushState(null, null, 'crearTabla');
-    window.location = 'crearTabla'
-    const popup = document.getElementById('popup')
-    popup.style.display = 'block';
-    document.body.classList.add('fondo-opaco');
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const openPopupButton = document.getElementById('abrir');
+    const closePopupButton = document.getElementById('closer');
+    const popup = document.getElementById('popup2');
+    const overlay = document.getElementById('overlay');
 
-function cerrarPopup() {
 
-        document.getElementById('popup').style.display = 'none';
-        history.back();
-        document.body.classList.remove('fondo-opaco');
-}
+    openPopupButton.addEventListener('click', function () {
+        popup.style.display = 'block';
+        overlay.style.display = 'block';
+    });
+
+    closePopupButton.addEventListener('click', function () {
+        popup.style.display = 'none';
+        overlay.style.display = 'none';
+    });
+
+    // Cierra el popup si el usuario hace clic fuera del popup
+    overlay.addEventListener('click', function () {
+        popup.style.display = 'none';
+        overlay.style.display = 'none';
+    });
+
+
+
+
+});
